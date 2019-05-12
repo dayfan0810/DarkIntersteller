259881adc09ce6ba5bfcb95dffe6efbf9b86a778
Fan Day <dayfan0810@gmail.com>
2019-05-13 00:56:50 +0800

    暂时不用自定义textview,官方有        <android.support.v7.widget.AppCompatTextView可以自适应文本大小

--- app/src/main/java/cn/intersteller/darkintersteller/custview/AutofitTextView.java
+++ app/src/main/java/cn/intersteller/darkintersteller/custview/AutofitTextView.java
@@ -1,10 +1,17 @@
 package cn.intersteller.darkintersteller.custview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
 
 public class AutofitTextView extends android.support.v7.widget.AppCompatTextView {
+    private float mDefaultTextSize;
+    private Paint mTextPaint;
+
     public AutofitTextView(Context context) {
         this(context, null);
     }
@@ -19,11 +26,51 @@ public class AutofitTextView extends android.support.v7.widget.AppCompatTextView
     }
 
     private void init() {
+        mTextPaint = new Paint();
+        mTextPaint.set(getPaint());
+        mDefaultTextSize = getTextSize();
     }
 
     @Override
     public void setTextSize(int unit, float size) {
         super.setTextSize(unit, size);
 
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        refitText(text.toString(), getWidth());
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        refitText(getText().toString(), getWidth());
+
+    }
+
+
+    public void refitText(String text, int textWidth) {
+        Log.e("refit", "refit:" + text + "width:" + textWidth);
+        if (textWidth > 0) {
+            int availableTextWidth = textWidth - getPaddingLeft() - getPaddingRight();
+            float tsTextSize = mDefaultTextSize;
+            mTextPaint.setTextSize(tsTextSize);
+            float length = mTextPaint.measureText(text);
+            while (length > availableTextWidth) {
+                tsTextSize--;
+                mTextPaint.setTextSize(tsTextSize);
+                length = mTextPaint.measureText(text);
+            }
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, tsTextSize);
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
